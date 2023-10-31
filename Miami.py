543863802070daa3bd12fe27b66583154cfce1a5
Winda Ayu Melati <148794156+windaayumelati@users.noreply.github.com>
2023-11-01 00:57:57 +0700

    Update Miami.py

--- Miami.py
+++ Miami.py
@@ -19,7 +19,7 @@ SUBCNTR_DI = st.number_input('Masukkan jarak ke subcenter terdekat')
 HWY_DIST = st.number_input('Masukkan jarak ke jalan raya terdekat (indikator kebisingan)')
 age = st.number_input('Masukkan usia struktur')
 avno60plus = st.number_input('Masukkan tingkat kebisingan pesawat')
-month_sold = st.number_input('Msukkan bulan penjualan')
+month_sold = st.number_input('Masukkan bulan penjualan')
 structure_quality = st.number_input('Masukan Kualitas struktur')
 
 predict = ''
@@ -28,6 +28,6 @@ if st.button('Estimasi Harga House MIAMI'):
     features = [[LATITUDE, LONGITUDE, PARCELNO, LND_SQFOOT, TOT_LVG_AREA, SPEC_FEAT_VAL, RAIL_DIST, OCEAN_DIST,
                  WATER_DIST, CNTR_DIST, SUBCNTR_DI, HWY_DIST, age, avno60plus, month_sold, structure_quality]]
     predict = model.predict(features)
-    st.write('Estimasi Harga House MIAMI: ', predict[0])
-    st.write('Estimasi Harga House MIAMI Dalam IDR (Juta): ', predict*19000)
+    st.write('Estimasi Harga House MIAMI (USD): ', predict[0])
+    st.write('Estimasi Harga House MIAMI Dalam ((IDR)): ', predict*19000)
     
